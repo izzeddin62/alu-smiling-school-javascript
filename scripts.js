979d3f5fed7679bd9f48e785d528273e7906254c
izzeddin62 <i.ishimwe@alustudent.com>
2023-12-10 15:54:19 +0200

    add 3

--- scripts.js
+++ scripts.js
@@ -60,12 +60,10 @@ $.ajax({
 $(document).ready(function () {
 
 
-    // Send GET request to the API
     $.ajax({
         url: "https://smileschool-api.alx-tools.com/popular-tutorials",
         method: "GET",
         success: function (response) {
-            // Handle the response data here
             console.log(response);
             const videos = response;
             $("#popular-videos-loading").remove();
@@ -105,25 +103,99 @@ $(document).ready(function () {
             )
 
 
-            const width = $(".card-carousel .carousel-inner").width();
-            const cardWidth = $(".card-carousel .carousel-inner  .carousel-item").outerWidth();
             let scrollPostion = 0;
-            $(".card-carousel .carousel-control-next").on("click", function () {
-                const width = $(".card-carousel .carousel-inner").width();
-                const cardWidth = $(".card-carousel .carousel-inner  .carousel-item").outerWidth();
+            $("#popular-videos .card-carousel .carousel-control-next").on("click", function () {
+                const cardWidth = $("#popular-videos .card-carousel .carousel-inner  .carousel-item").outerWidth();
                 if (scrollPostion >= (cardWidth * response.length) - cardWidth) {
                     return;
                 }
                 scrollPostion += cardWidth
-                $(".card-carousel .carousel-inner").animate({ scrollLeft: scrollPostion }, 400);
+                $("#popular-videos .card-carousel .carousel-inner").animate({ scrollLeft: scrollPostion }, 400);
             });
         
-            $(".card-carousel .carousel-control-prev").on("click", function () {
+            $("#popular-videos .card-carousel .carousel-control-prev").on("click", function () {
                 if (scrollPostion <= 0) {
                     return;
                 }
-                const width = $(".card-carousel .carousel-inner").width();
-                const cardWidth = $(".card-carousel .carousel-inner  .carousel-item").outerWidth();
+                const width = $("#popular-videos .card-carousel .carousel-inner").width();
+                const cardWidth = $("#popular-videos .card-carousel .carousel-inner  .carousel-item").outerWidth();
+                scrollPostion -= cardWidth
+                $("#popular-videos .card-carousel .carousel-inner").animate({ scrollLeft: scrollPostion }, 400);
+            });
+
+            
+        },
+        error: function (error) {
+            console.error(error);
+        }
+    });
+});
+
+
+
+
+$(document).ready(function () {
+
+    $.ajax({
+        url: "https://smileschool-api.alx-tools.com/latest-videos",
+        method: "GET",
+        success: function (response) {
+
+            console.log(response, '====++++======');
+            const videos = response;
+            $("#latest-videos-loading").remove();
+            $("#latest-videos").append(
+                $('<div class="carousel slide card-carousel" data-ride="carousel">').append(
+                    $('<div class="carousel-inner d-flex">').append(
+                        response.map(video => {
+                            return $('<div class="card border-0 carousel-item d-block m-0">').append(
+                                $(`<img src="${video.thumb_url}" width="288" height="224" class="card-img-top" alt="Card Image">`),
+                                $('<div class="card-body px-0" style="border: none;">').append(
+                                    $('<h5 class="card-title">').text(video.title),
+                                    $('<p class="card-text" style="opacity: 0.7;">').text(video["sub-title"]),
+                                ),
+                                $('<div class="card-footer px-0 bg-white">').append(
+                                    $('<div class="d-flex" style="gap: 20px;">').append(
+                                        $(`<img src="${video.author_pic_url}" class="rounded-circle" width=30 height=30 alt="">`),
+                                        $('<p style="color: rgba(194, 113, 255, 1);">').text(video.author)
+                                    ),
+                                    $('<div>').append(
+                                        Array(video.star).fill(0).map(() => {
+                                            return $('<img src="images/star_on.png" alt="">')
+                                        })
+                                    )
+                                )
+                            )
+                        })
+                    ),
+                    $('<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">').append(
+                        $('<span class="carousel-control-prev-icon" aria-hidden="true">'),
+                        $('<span class="sr-only">').text("Previous")
+                    ),
+                    $('<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">').append(
+                        $('<span class="carousel-control-next-icon" aria-hidden="true">'),
+                        $('<span class="sr-only">').text("Next")
+                    )
+                )
+            )
+
+
+            let scrollPostion = 0;
+            $("#latest-videos .card-carousel .carousel-control-next").on("click", function () {
+                const cardWidth = $("#latest-videos .card-carousel .carousel-inner  .carousel-item").outerWidth();
+                if (scrollPostion >= (cardWidth * response.length) - cardWidth) {
+                    return;
+                }
+                scrollPostion += cardWidth
+                $("#latest-videos .card-carousel .carousel-inner").animate({ scrollLeft: scrollPostion }, 400);
+            });
+        
+            $("#latest-videos .card-carousel .carousel-control-prev").on("click", function () {
+                if (scrollPostion <= 0) {
+                    return;
+                }
+                const width = $("#latest-videos .card-carousel .carousel-inner").width();
+                const cardWidth = $("#latest-videos .card-carousel .carousel-inner  .carousel-item").outerWidth();
                 scrollPostion -= cardWidth
                 $(".card-carousel .carousel-inner").animate({ scrollLeft: scrollPostion }, 400);
             });
@@ -131,7 +203,6 @@ $(document).ready(function () {
             
         },
         error: function (error) {
-            // Handle any errors that occur during the request
             console.error(error);
         }
     });
